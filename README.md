11ba4b50ad52c288799ba31909a06ec46867359f
KaylaS1 <kalas1@hotmail.com>
2024-06-22 15:53:34 -0400

    Python 2 Challenge 3 complete

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # customer_banking
-Module 3 Python Challenge
+Module 3 Python Challenge - Customer Banking function
+
+# To run, execute
+python customer_banking.py
+
+# User input
+* To create a Savings account, it asks for balance amount, interest rate and number of months.
+* To create a CD account, it asks for balance amount, interest rate and number of months.
+
+# This is where I got sections of my code
+1. Account.py was provided to us.
+1. In savings_account.py, the outline of the create_savings_account function was provided.
+1. In cd_account.py, the outline of the create_cd_account was provided.
+1. In customer_banking.py, all instructions were provided as comments.
+1. I got "try except ValueError" code from a Google search.
--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,15 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from cd_account import create_cd_account
+from savings_account import create_savings_account
+
+# Formatted output print function
+def print_output(account_type, balance, interest):
+    printed_dashes = "-" * 50
+    print(printed_dashes)
+    print("Your", account_type, "account balance is $", format(balance, ',.2f'))
+    print("Your", account_type, "interest earned is $", format(interest, ',.2f'))
+    print(printed_dashes)
 
 # Define the main function
 def main():
@@ -9,21 +19,67 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
+    while True:
+        savings_balance = input("Please enter savings balance as number with decimals, no currency or commas: ")
+        try:
+            savings_balance = float(savings_balance)
+            break
+        except ValueError:
+            return False
+
+    while True:
+        savings_interest = input("Please enter savings interest rate as number with decimals, no percentage or commas: ")
+        try:
+            savings_interest = float(savings_interest)
+            break
+        except ValueError:
+            return False
+
+    while True:
+        savings_maturity = input("Please enter number of months with no decimals: ")
+        if savings_maturity.isdigit():
+            savings_maturity = int(savings_maturity)
+            break
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
+    print_output("Savings", updated_savings_balance, interest_earned)
+        
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    # This code below can be a function also, as it gets executed the same for both types of accounts - CD and Savings
+    while True:
+        cd_balance = input("Please enter CD balance as number with decimals, no currency or commas: ")
+        try:
+            cd_balance = float(cd_balance)
+            break
+        except ValueError:
+            return False
+
+    while True:
+        cd_interest = input("Please enter CD interest rate as number with decimals, no percentage or commas: ")
+        try:
+            cd_interest = float(cd_interest)
+            break
+        except ValueError:
+            return False
+
+    while True:
+        cd_maturity = input("Please enter number of months with no decimals: ")
+        if cd_maturity.isdigit():
+            cd_maturity = int(cd_maturity)
+            break
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print_output("CD", updated_cd_balance, interest_earned)
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
